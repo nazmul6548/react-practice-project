c9ce05150a582577e10824aabdffecdb51dc3d21
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-03-08 10:46:20 +0600

    task

---
+++ src/home/home.jsx
@@ -0,0 +1,10 @@
+import './home.css'
+
+function Home() {
+    return (
+        <div>
+            <h1 className='designH1'>I am from Chittagong</h1>
+        </div>
+    )
+}
+export default Home;
