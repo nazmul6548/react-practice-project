c367fde5b93350fe4b9ed3482a0e3ab5a1b77b64
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-03-08 11:56:06 +0600

    task

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,10 +1,20 @@
 import './home.css'
 
-function Home() {
+const  Home = ()  =>{
+    function anything(){
+        // alert('Please enter')
+        const service = document.getElementById('service-combo');
+        service.innerText ="hello i am new"
+    }
     return (
         <div>
-            <h1 className='designH1'>I am from Chittagong</h1>
+            <h1 id='service-combo'>i am from chittagong</h1>
+            <button onClick={anything} className='clickbtn'>click me</button>
+
+    
         </div>
+            
+        
     )
 }
 export default Home;
