5b4f81b10eba6a2bea76ba558eb2d72a8314a688
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-03-08 17:29:46 +0600

    task

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,29 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Home></Home>
-    
+    <>
+     <Home></Home>
+    <Argument></Argument>
+    <Argument></Argument>
+    <Argument></Argument>
+    </>
+   
     
   )
+}
+
+function Argument () {
+  
+   const age = 25;
+   const money = 20;
+   const person = {
+    name: 'John',
+    age:12
+   }
+   return (
+    <h3>i am {person.name} i am {age} years old,i have more {money} money </h3>
+   )
+
 }
 
 export default App
